209a8fdf18a6f4642c90cfc4bc5b4c0562805676
erhansennx <erhanfatih33@gmail.com>
2023-04-28 15:16:28 +0300

    Added user keyboard listener and server response check done.

--- app/src/main/java/com/erhansen/githubprofileviewer/MainActivity.kt
+++ app/src/main/java/com/erhansen/githubprofileviewer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.erhansen.githubprofileviewer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
 import com.erhansen.githubprofileviewer.databinding.ActivityMainBinding
 import com.erhansen.githubprofileviewer.service.UserProfileApi
 import com.erhansen.githubprofileviewer.service.UserProfileApiService
@@ -19,18 +20,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityMainBinding.root)
 
         val userProfileApi = UserProfileApiService.getInstance().create(UserProfileApi::class.java)
-        GlobalScope.launch {
-            val result = userProfileApi.getUserInformation()
-            if (result != null) {
-                println("Login : ${result.body()!!.login}")
-                println("avatar_url : ${result.body()!!.avatarUrl}")
-                println("name : ${result.body()!!.name}")
-                println("location : ${result.body()!!.location}")
-                println("followers : ${result.body()!!.followers}")
-                println("following : ${result.body()!!.following}")
-            }
 
+        with(activityMainBinding) {
+            searchUser.setOnEditorActionListener { v, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    if (!searchUser.text.isNullOrEmpty()) {
+                        GlobalScope.launch {
+                            val result = userProfileApi.getUserInformation(searchUser.text.toString())
+                            if (result.isSuccessful) {
+                                println("Login : ${result.body()!!.login}")
+                                println("avatar_url : ${result.body()!!.avatarUrl}")
+                                println("name : ${result.body()!!.name}")
+                                println("location : ${result.body()!!.location}")
+                                println("followers : ${result.body()!!.followers}")
+                                println("following : ${result.body()!!.following}")
+                            } else if (result.code() == 404) {
+                                // user not found!
+                            }
+                        }
+                    }
+                    return@setOnEditorActionListener true
+                } else {
+                    return@setOnEditorActionListener false
+                }
+            }
         }
+
+
 
 
     }
--- app/src/main/java/com/erhansen/githubprofileviewer/service/UserProfileApi.kt
+++ app/src/main/java/com/erhansen/githubprofileviewer/service/UserProfileApi.kt
@@ -3,10 +3,11 @@ package com.erhansen.githubprofileviewer.service
 import com.erhansen.githubprofileviewer.model.UserProfileModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface UserProfileApi {
 
-    @GET("/users/erhansennx")
-    suspend fun getUserInformation(): Response<UserProfileModel>
+    @GET("/users/{username}")
+    suspend fun getUserInformation(@Path("username") username: String): Response<UserProfileModel>
 
 }
