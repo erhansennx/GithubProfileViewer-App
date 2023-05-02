82b706f6c16504200c0eabf24d5f6d153cf8022f
erhansennx <erhanfatih33@gmail.com>
2023-05-02 22:01:03 +0300

    Repositories operations are complete

--- app/src/main/java/com/erhansen/githubprofileviewer/adapter/RepositoriesAdapter.kt
+++ app/src/main/java/com/erhansen/githubprofileviewer/adapter/RepositoriesAdapter.kt
@@ -1,4 +1,40 @@
 package com.erhansen.githubprofileviewer.adapter
 
-class RepositoriesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.erhansen.githubprofileviewer.R
+import com.erhansen.githubprofileviewer.model.RepositoriesModel
+import de.hdodenhof.circleimageview.CircleImageView
+
+class RepositoriesAdapter(private val repositories: RepositoriesModel) : RecyclerView.Adapter<RepositoriesAdapter.ItemHolder>(){
+
+    class ItemHolder(itemView: View) : ViewHolder(itemView) {
+        val repoName: TextView = itemView.findViewById(R.id.repoName)
+        val descriptionText: TextView = itemView.findViewById(R.id.descriptionText)
+        val languageColor: CircleImageView = itemView.findViewById(R.id.languageColor)
+        val languageText: TextView = itemView.findViewById(R.id.languageText)
+        val starText: TextView = itemView.findViewById(R.id.starText)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_repositories, parent, false)
+        return ItemHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return repositories.size
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        holder.repoName.text = repositories[position].name
+        holder.descriptionText.text = repositories[position].description
+        holder.languageText.text = repositories[position].language
+        holder.starText.text = repositories[position].stargazersCount.toString()
+    }
+
+
 }
--- app/src/main/java/com/erhansen/githubprofileviewer/fragments/RepositoriesFragment.kt
+++ app/src/main/java/com/erhansen/githubprofileviewer/fragments/RepositoriesFragment.kt
@@ -6,16 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.erhansen.githubprofileviewer.adapter.RepositoriesAdapter
 import com.erhansen.githubprofileviewer.databinding.FragmentRepositoriesBinding
+import com.erhansen.githubprofileviewer.model.RepositoriesModel
 import com.erhansen.githubprofileviewer.utils.CreateFragment
 
 class RepositoriesFragment : Fragment() {
 
-
+    private lateinit var repositories: RepositoriesModel
+    private lateinit var repositoriesAdapter: RepositoriesAdapter
     private lateinit var fragmentRepositoriesBinding: FragmentRepositoriesBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentRepositoriesBinding = FragmentRepositoriesBinding.inflate(layoutInflater)
+
+        repositories = arguments?.getParcelable<RepositoriesModel>("repositories") ?: RepositoriesModel()
+        repositoriesAdapter = RepositoriesAdapter(repositories)
 
         return fragmentRepositoriesBinding.root
     }
@@ -24,7 +30,7 @@ class RepositoriesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         with(fragmentRepositoriesBinding) {
-
+            repoRecycler.adapter = repositoriesAdapter
         }
 
     }
