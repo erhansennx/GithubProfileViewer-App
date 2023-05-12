d29f4b788d7dd84478dd9678dd52b89a1bce8534
erhansennx <erhanfatih33@gmail.com>
2023-05-12 20:03:14 +0300

    TabLayout style implemented

--- app/src/main/java/com/erhansen/githubprofileviewer/fragments/OverviewFragment.kt
+++ app/src/main/java/com/erhansen/githubprofileviewer/fragments/OverviewFragment.kt
@@ -6,12 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.bumptech.glide.Glide
 import com.erhansen.githubprofileviewer.R
 import com.erhansen.githubprofileviewer.adapter.RepositoriesAdapter
+import com.erhansen.githubprofileviewer.adapter.ViewPagerAdapter
 import com.erhansen.githubprofileviewer.databinding.FragmentOverviewBinding
 import com.erhansen.githubprofileviewer.model.RepositoriesModel
 import com.erhansen.githubprofileviewer.utils.CreateFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 class OverviewFragment : Fragment() {
 
@@ -20,12 +26,14 @@ class OverviewFragment : Fragment() {
     private var avatarURL: String ?= null
     private lateinit var repositories: RepositoriesModel
     private lateinit var repositoriesAdapter: RepositoriesAdapter
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
     private lateinit var fragmentRepositoriesBinding: FragmentOverviewBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentRepositoriesBinding = FragmentOverviewBinding.inflate(layoutInflater)
 
         repositories = arguments?.getParcelable<RepositoriesModel>("repositories") ?: RepositoriesModel()
+        viewPagerAdapter = ViewPagerAdapter(requireActivity())
         name = arguments?.getString("name",null)
         username = arguments?.getString("username",null)
         avatarURL = arguments?.getString("avatarURL",null)
@@ -47,6 +55,25 @@ class OverviewFragment : Fragment() {
             backButton.setOnClickListener {
                 val createFragment = CreateFragment(requireActivity() as AppCompatActivity, SearchUserFragment(), null)
             }
+
+            viewPager.adapter = viewPagerAdapter
+            tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    viewPager.currentItem = tab?.position!!
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) { }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) { }
+
+            })
+            viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    tabLayout.getTabAt(position)?.select()
+                }
+            })
+
         }
 
     }
--- app/src/main/java/com/erhansen/githubprofileviewer/fragments/RepositoriesFragment.kt
+++ app/src/main/java/com/erhansen/githubprofileviewer/fragments/RepositoriesFragment.kt
@@ -1,18 +1,26 @@
 package com.erhansen.githubprofileviewer.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.erhansen.githubprofileviewer.adapter.RepositoriesAdapter
 import com.erhansen.githubprofileviewer.databinding.FragmentRepositoriesBinding
+import com.erhansen.githubprofileviewer.model.RepositoriesModel
 
 class RepositoriesFragment : Fragment() {
 
+    private lateinit var repositories: RepositoriesModel
+    private lateinit var repositoriesAdapter: RepositoriesAdapter
     private lateinit var fragmentRepositoriesBinding: FragmentRepositoriesBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentRepositoriesBinding = FragmentRepositoriesBinding.inflate(layoutInflater)
 
+        repositories = arguments?.getParcelable<RepositoriesModel>("repositories") ?: RepositoriesModel()
+        repositoriesAdapter = RepositoriesAdapter(requireContext(), repositories)
+        //fragmentRepositoriesBinding.repositoriesRecycler.adapter = repositoriesAdapter
 
 
         return fragmentRepositoriesBinding.root
